93089142d035ff05789cf8c0f84dc47a1490b835
ndtribk <ndtribk@gmail.com>
2026-05-28 16:14:05 +0700

    update

---
+++ secure-chat/server/init.py
@@ -0,0 +1,11 @@
+"""
+Secure Messaging System Server Package
+
+This package contains:
+
+- authentication logic
+- websocket relay server
+- database layer
+- API routes
+- security/session handling
+"""
---
+++ secure-chat/server/routes/init.py
@@ -0,0 +1,8 @@
+"""
+Route package for Secure Messaging System.
+
+Contains:
+- authentication routes
+- public key directory endpoints
+- future session management APIs
+"""
